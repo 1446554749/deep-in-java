f1a2202b4d172f9aaa7af18b4c033b25b517b2b3
liming <1446554749@qq.com>
2022-09-02 16:10:21 +0800

    We really don't want Git to track this anymore!

---
+++ async-model/src/test/java/JdkFutureTest.java
@@ -0,0 +1,32 @@
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Slf4j
+public class JdkFutureTest {
+
+    public static void main(String[] args) {
+        final ExecutorService pool = Executors.newFixedThreadPool(1);
+
+        final Future<Integer> future = pool.submit(() -> {
+            log.debug("compute");
+            Thread.sleep(1000);
+            return 1;
+        });
+
+        try {
+            log.debug("wait for result");
+            Integer res = future.get();
+            log.debug("result is {}", res);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            pool.shutdown();
+        }
+
+    }
+
+}
